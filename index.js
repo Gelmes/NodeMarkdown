ba702b9b0c532c71c7a20ec79a3d948f334088ca
Gelmes <marco.firsteye@gmail.com>
2021-04-07 19:43:28 -0700

    Render markdown found in markdown folder

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const app = express();
+var md = require('markdown-it')({
+    html: true,
+    linkify: true,
+    typographer: true
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = 3000;
+const markdownTemplate = '/public/markdown.html';
+const markdownFolder = '/markdown/';
+
+app.use(express.static('public'));
+// app.get('/', (req, res) => {
+//     res.send('Request ' + req);
+// })
+
+app.get('/:file', function(req, res) {
+    console.log("Params", req.params);
+    var html = fs.readFileSync(__dirname + markdownTemplate, 'utf8');
+    var markup = fs.readFileSync(__dirname + markdownFolder + req.params.file, 'utf8');
+    var $ = cheerio.load(html);
+    // var scriptNode = '<script>alert("script appended!");</script>';
+    var render = md.render(markup);
+    $('body').append(render);
+    res.send($.html());
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ markdown/readme
@@ -0,0 +1,2 @@
+# NodeMarkdown
+A simple web server with the goal of rendering markdown pages
